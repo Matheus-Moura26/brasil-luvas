e860818d26cdaa3f33bb2729247589148af4318f
Matheus-Moura26 <moura.matheus26@gmail.com>
2023-05-05 19:44:19 -0300

    Adicinando o texto da pagina Sobre

--- pages/sobre.js
+++ pages/sobre.js
@@ -4,9 +4,27 @@ function Sobre() {
     return (
         <div>
 
-                <main>
-                    <h1>Sobre</h1>
-                </main>
+            <main>
+                <p>
+                    A Brasil Luvas é uma empresa líder no mercado de equipamentos de segurança e ferramentas para construção civil. Atuando há mais de 20 anos, a empresa é especializada na representação de produtos de alta qualidade, com foco na satisfação dos clientes.
+                </p>
+
+                <p>
+                    Fundada em 26 de junho de 2000, a Brasil Luvas tem como missão fornecer os melhores equipamentos de segurança e ferramentas para construção civil em geral, com um atendimento personalizado e um alto padrão de qualidade. A empresa tem uma equipe de profissionais altamente capacitados e comprometidos com a satisfação dos clientes.
+                </p>
+
+                <p>
+                    Sediada em Goiás e atende também o Distrito Federal. Além disso, a empresa oferece entrega em todo o Brasil, com o frete por conta do cliente e a entrega feita através de transportadora, garantindo a segurança e a rapidez na entrega dos produtos.
+                </p>
+
+                <p>
+                    A ampla variedade de produtos oferecidos pela Brasil Luvas inclui equipamentos de proteção individual (EPIs), como luvas, capacetes, botas, óculos de proteção, entre outros, além de ferramentas para construção civil, como serras, martelos, furadeiras, entre outras. Todos os produtos são de marcas reconhecidas no mercado, garantindo a qualidade e a segurança para os usuários.
+                </p>
+
+                <p>
+                    Com a Brasil Luvas, os clientes podem contar com um atendimento personalizado e eficiente, além de produtos de alta qualidade e preços competitivos. A empresa está sempre em busca de novas soluções e tecnologias para oferecer aos clientes o que há de melhor no mercado de equipamentos de segurança e ferramentas para construção civil.
+                </p>
+            </main>
         </div>
     )
 }
